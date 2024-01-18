246bdd65d2e6278e72ecf2594831a1be97331598
Raul Madero <127880270+Raul-madero@users.noreply.github.com>
2024-01-18 16:27:05 +0000

    Cambio peticion post y delete

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from models import People, Planets
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,14 +36,98 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
+@app.route('/people', methods=['GET'])
+def get_people():
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        'id': People.id,
+        'name': People.name,
+        'height': People.height,
+        'mass': People.mass,
+        'hair_color': People.hair_color,
+        'skin_color': People.skin_color,
+        'eye_color': People.eye_color,
+        'birth_year': People.birth_year,
+        'gender': People.gender,
+        'homeworld': People.homeworld
     }
-
     return jsonify(response_body), 200
+
+@app.route('/planets', methods=['GET'])
+def get_people():
+    response_body = {
+        'id': Planets.id,
+        'name': Planets.name,
+        'rotation_period': Planets.rotation_period,
+        'orbital_period': Planets.orbital_period,
+        'diameter': Planets.diameter,
+        'climate': Planets.climate,
+        'gravity': Planets.gravity,
+        'terrain': Planets.terrain,
+        'surface_water': Planets.surface_water,
+        'population': Planets.population
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites', methods=['GET'])
+def get_favorites():
+    planet_favorite = User.planet_favorite
+    people_favorite = User.people_favorite
+    favorites = Planets.query.filter(
+        (Planets.id.ilike(planet_favorite))
+    ) & People.query.filter(
+        (People.id.ilike(people_favorite))
+    )
+    return jsonify(favorites), 200
+
+@app.route('/user/favorites/<int:people_id>')
+def get_one_person(id):
+        person = People.query.get_or_404(id)
+        return jsonify(person), 200
+
+@app.route('/user/favorites/<int:planets_id>')
+def get_one_planet(id):
+        planet = Planets.query.get_or_404(id)
+        return jsonify(planet), 200
+
+@app.route('/user', methods=['GET'])
+def get_users():
+    response_body = {
+        "id": User.id,
+        "email": User.email,
+        "password": User.password,
+        "is_active": User.is_active
+    }
+    return jsonify(response_body), 200
+
+@app.route('/favorite/planets/<int:planets_id>', methods=['POST', 'DELETE'])
+def add_or_remove_planet_to_favorites(user_id, planet_id):
+     user = User.query.get_or_404(user_id)
+     if request.method == 'DELETE': 
+          planeta_eliminado = user.planet_favorite(planet_id)
+          db.session.delete(planeta_eliminado)
+          db.session.comit()
+          return jsonify({"message": "Planeta eliminado de favoritos"})
+     else:
+        nuevo_Favorito = user(planet_id=planet_id)
+        db.session.add(nuevo_Favorito)
+        db.session.commit()
+        return jsonify({"message": "Planeta añadido a favoritos"})
+     
+     
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST', 'DELETE'])
+def add_or_remove_planet_to_favorites(user_id, people_id):
+    user = User.query.get_or_404(user_id)
+    if request.method == 'DELETE': 
+          persona_eliminada = user.people_favorite(people_id)
+          db.session.delete(persona_eliminada)
+          db.session.comit()
+          return jsonify({"message": "Persona eliminada de favoritos"})
+    else:
+        nuevo_Favorito = user(people_id=people_id)
+        db.session.add(nuevo_Favorito)
+        db.session.commit()
+        return jsonify({"message": "Persona añadida a favoritos"})
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,11 +2,40 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.string(45), nullable=False, unique=False)
+    height = db.Column(db.String(45), nullable=False, unique=False)
+    mass = db.Column(db.String(45), nullable=False, unique=False)
+    hair_color: db.Column(db.String(45), nullable=False, unique=False)
+    skin_color: db.Column(db.String(45), nullable=False, unique=False)
+    eye_color: db.Column(db.String(45), nullable=False, unique=False)
+    birth_year: db.Column(db.String(45), nullable=False, unique=False)
+    gender: db.Column(db.String(45), nullable=False, unique=False)
+    homeworld: db.Column(db.String(45), nullable=False, unique=False)
+
+class Planets(db.Model):
+    id: db.Column(db.Integer, primary_key=True)
+    name: db.Column(db.String(45), nullable=False, unique=False)
+    rotation_period: db.Column(db.String(45), nullable=False, unique=False)
+    orbital_period: db.Column(db.String(45), nullable=False, unique=False)
+    diameter: db.Column(db.String(45), nullable=False, unique=False)
+    climate: db.Column(db.String(45), nullable=False, unique=False)
+    gravity: db.Column(db.String(45), nullable=False, unique=False)
+    terrain: db.Column(db.String(45), nullable=False, unique=False)
+    surface_water: db.Column(db.String(45), nullable=False, unique=False)
+    population: db.Column(db.String(45), nullable=False, unique=False)
+    
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    planet_favorite = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    colaborador = db.relationship('Planets')
+    people_favorite = db.Column(db.Integer, db.ForeignKey('people.id'))
+    colaborador = db.relationship('People')
 
     def __repr__(self):
         return '<User %r>' % self.username
